8cb23daf4274e140e0f09cb9a6ea3232089c55d1
lizard123137 <lizard123137@gmail.com>
2024-12-04 00:38:30 +0100

    added first article

--- backend/controllers/api_Articles.cc
+++ backend/controllers/api_Articles.cc
@@ -1,3 +1,5 @@
+#include <fstream>
+
 #include "api_Articles.h"
 
 using namespace api;
@@ -23,9 +25,28 @@ void Articles::show(
 ) {
     LOG_DEBUG << "Requested article: " << slug;
 
-    Json::Value ret;
-    ret["slug"] = slug;
+    std::string articles_dir = "../articles/";
+    std::string file_path = articles_dir + slug + ".md";
 
-    auto resp = HttpResponse::newHttpJsonResponse(ret);
-    callback(resp);
+    LOG_DEBUG << "Trying to access file at: " << file_path;
+
+    if (!std::filesystem::exists(file_path)) {
+        auto response = HttpResponse::newHttpResponse();
+        response->setStatusCode(HttpStatusCode::k404NotFound);
+        response->setBody("Article with that slug doesn't exist");
+        callback(response);
+        return;
+    }
+
+    std::ifstream file(file_path);
+    std::ostringstream content;
+    content << file.rdbuf();
+    file.close();
+
+    Json::Value data;
+    data["slug"] = slug;
+    data["content"] = content.str();
+
+    auto response = HttpResponse::newHttpJsonResponse(data);
+    callback(response);
 }
