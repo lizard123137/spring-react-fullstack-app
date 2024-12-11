14f6d440f38e0f63632452c4ea9f6d3424b6530a
lizard123137 <lizard123137@gmail.com>
2024-12-11 16:43:22 +0100

    started work on routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import NotFound from './pages/NotFound/NotFound';
+import Home from './pages/Home/Home';
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <NotFound />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
---
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="h-screen flex flex-col justify-center items-center">
+            <h1 className="text-9xl">404</h1>
+            <h2 className="text-xl text-gray-600">Not Found!</h2>
+        </div>
+    )
+}
