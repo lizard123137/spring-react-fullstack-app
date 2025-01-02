2355c5d76c3d40456e9e2f661a39b63f6c2c2f4f
lizard123137 <lizard123137@gmail.com>
2025-01-02 08:46:25 +0100

    changed backend to spring

---
+++ backend/src/test/kotlin/com/dev/caps/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dev.caps.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
